b5ccfda0be5a60ba892797bfe504b16fbcbf5a40
mikias-worash <mikias.worash.a@gmail.com>
2023-05-20 09:26:04 +0300

    added mongo conn

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,38 @@
 import asyncHanlder from 'express-async-handler'
 
 // @desc Auth User / Set token
-// router POSR /api/users/auth
+// router POST /api/users/auth
 // @access Public
 const authUser = asyncHanlder(async (req, res) => {
-  res.status(401)
-  throw new Error('something went wrong')
-
   res.status(200).json({ message: 'Authentication successful' })
 })
 
-export { authUser }
+// @desc Register a new user
+// router POST /api/users
+// @access Public
+const registerUser = asyncHanlder(async (req, res) => {
+  res.status(200).json({ message: 'Registration successful' })
+})
+
+// @desc Logout User
+// router POST /api/users/logout
+// @access Public
+const logoutUser = asyncHanlder(async (req, res) => {
+  res.status(200).json({ message: 'Logout successful' })
+})
+
+// @desc Get User Profile
+// router GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHanlder(async (req, res) => {
+  res.status(200).json({ message: 'Get user profile successful' })
+})
+
+// @desc Update User Profile
+// router PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHanlder(async (req, res) => {
+  res.status(200).json({ message: 'Update user profile successful' })
+})
+
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile }
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
+import colors from 'colors'
 const port = process.env.PORT || 5000
 import userRoutes from './routes/userRoutes.js'
+
+connectDB()
 
 const app = express()
 
@@ -12,4 +16,4 @@ app.use('/api/users', userRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server listening on ${port}`))
+app.listen(port, () => console.log(`Server listening on ${port}`.yellow.bold))
