400b9a65fb797d98a75a093e9044be981295ed1f
mikias-worash <mikias.worash.a@gmail.com>
2023-05-30 20:29:02 +0300

    added middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,13 @@
+import asyncHanlder from 'express-async-handler'
+
 // @desc Auth User / Set token
 // router POSR /api/users/auth
 // @access Public
-const authUser = (req, res) => {
+const authUser = asyncHanlder(async (req, res) => {
+  res.status(401)
+  throw new Error('something went wrong')
+
   res.status(200).json({ message: 'Authentication successful' })
-}
+})
 
 export { authUser }
