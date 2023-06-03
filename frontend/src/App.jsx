464aa09b788e887fdfee16ef759da89db6a1147c
mikias-worash <mikias.worash.a@gmail.com>
2023-06-03 19:27:21 +0300

    setup react-bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+
 function App() {
-  return <h1>App</h1>
+  return (
+    <>
+      <Header />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
+    </>
+  )
 }
 
 export default App
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,7 @@
+import Hero from './Hero'
+
+const HomeScreen = () => {
+  return <Hero />
+}
+
+export default HomeScreen
